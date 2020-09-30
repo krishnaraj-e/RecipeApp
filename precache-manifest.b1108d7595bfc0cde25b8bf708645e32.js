e3c8cfcb5f69e0a4eef0c278739da5edc2b13329
krishnaraj-e <krishnarey972@gmail.com>
2020-10-01 04:19:02 +0530

    Updates

---
+++ precache-manifest.b1108d7595bfc0cde25b8bf708645e32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a0fc40267b36da30a7f460354d0741e",
+    "url": "/RecipeApp/index.html"
+  },
+  {
+    "revision": "e9186926a7273664ea9c",
+    "url": "/RecipeApp/static/css/main.3ee2894b.chunk.css"
+  },
+  {
+    "revision": "f3ed12bffeb81b8fce92",
+    "url": "/RecipeApp/static/js/2.3fd06d78.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RecipeApp/static/js/2.3fd06d78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9186926a7273664ea9c",
+    "url": "/RecipeApp/static/js/main.5af352fd.chunk.js"
+  },
+  {
+    "revision": "2569b9ec1ba1142dccbc",
+    "url": "/RecipeApp/static/js/runtime-main.8f6c811b.js"
+  }
+]);
